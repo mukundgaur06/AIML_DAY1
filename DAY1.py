9f68798f95d8b5a096be5eb55b4f3de508861659
mukundgaur06 <mukundgaur06@gmail.com>
2026-01-30 02:10:54 +0530

    Day_1, did some basic matplotlib and pandas file accessing and visual representation

---
+++ DAY1.py
@@ -0,0 +1,35 @@
+#%%
+medical_charges_url = 'https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv'
+from urllib.request import urlretrieve
+urlretrieve(medical_charges_url,'medical.csv')
+
+import pandas as pd
+medical_df = pd.read_csv('medical.csv')
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+print(medical_df)
+print(medical_df.head())
+print(medical_df.info())
+print(medical_df.describe())
+import jovian
+import plotly.express as px
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style('darkgrid')
+matplotlib.rcParams['font.size'] = 14
+matplotlib.rcParams['figure.figsize'] = (10, 6)
+matplotlib.rcParams['figure.facecolor'] = '#00000000'
+print("Howdi!!??")
+print(medical_df.age.describe())
+plt.show()
+fig = px.histogram(medical_df, x='age', marginal='box', nbins=47, title='Distribution of Age')
+fig.update_layout(bargap=0.1)
+fig.show()
+fig = px.histogram(medical_df, x='bmi', marginal='box', color_discrete_sequence=['red'], title='Distribution of BMI (Body Mass Index)')
+fig.update_layout(bargap=0.1)
+fig.show()
+
+# %%
